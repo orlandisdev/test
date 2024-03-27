1e582cfd55a8340fa94352c313bdd11cd453dedc
Orlandis Cuevas <orlandis.cuevas@alten.es>
2024-03-27 16:09:56 +0100

    Añadiendo una libreria de componentes

--- resources/js/Pages/Home.jsx
+++ resources/js/Pages/Home.jsx
@@ -1,14 +1,18 @@
 import Submenu from '@/Components/ui/submenu';
 import Guest from '@/Layouts/GuestLayout';
 import { Link, Head } from '@inertiajs/react';
-
+import {
+    HoverCard,
+    HoverCardContent,
+    HoverCardTrigger,
+  } from "@/components/ui/hover-card"
 export default function Welcome({ auth, laravelVersion, phpVersion }) {
     return (
         <>
             <Guest>
                 <Head title="Inicio" />
 
-                <main className="ml-3">
+                <main className="ml-3 bg-red-600">
 
                     <h1 className='font-bold text-3xl block'>
                         Facturas
@@ -17,6 +21,14 @@ export default function Welcome({ auth, laravelVersion, phpVersion }) {
                     <Submenu />
 
                 </main>
+
+                <HoverCard>
+  <HoverCardTrigger>Hover</HoverCardTrigger>
+  <HoverCardContent>
+    The React Framework – created and maintained by @vercel.
+  </HoverCardContent>
+</HoverCard>
+
             </Guest>
         </>
     );
